cd3ce7e8dafb62533d325b0f38d0c8fde0c099ea
Rakesh Das <rakeshdaskowshik@gmail.com>
2015-08-16 14:10:09 -0500

    Redirection works

--- src/bg/background.js
+++ src/bg/background.js
@@ -7,7 +7,7 @@ chrome.webRequest.onBeforeRequest.addListener(function (details) {
 function detectRedirect(details) {
     if (!!details.url.match('http://www.amazon.com') || !!details.url.match('amazon.com')) {
             return {
-                redirectUrl: 'http://www.amazon.com/ref=as_li_ss_tl?_encoding=UTF8&camp=1789&creative=390957&linkCode=ur2&tag=linustechtips-20&linkId=XVVUX6TEDQOUT4OZ'
+                redirectUrl: 'http://www.amazon.com/dp/B00A2T6X0K/?tag=headpreviehea-20'
             };
         };
 }
