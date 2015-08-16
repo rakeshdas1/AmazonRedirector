d3f288d500f0a895f65296cb7a1989cf1c3b4568
Rakesh Das <rakeshdaskowshik@gmail.com>
2015-08-16 14:16:54 -0500

    Organization

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ chrome.webRequest.onBeforeRequest.addListener(function (details) {
 function detectRedirect(details) {
     if (!!details.url.match('http://www.amazon.com') || !!details.url.match('amazon.com')) {
             return {
-                redirectUrl: 'http://www.amazon.com/dp/B00A2T6X0K/?tag=headpreviehea-20'
+                redirectUrl: 'http://www.amazon.com/?_encoding=UTF8&camp=1789&creative=390957&linkCode=ur2&tag=linustechtips-20'
             };
         };
 }
