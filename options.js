53058f6e4d94fbf7e57b2535a3b397d90ca11bbd
Rakesh Das <rakeshdaskowshik@gmail.com>
2015-08-16 23:53:41 -0500

    Options improvments

--- options.js
+++ options.js
@@ -1,4 +1,19 @@
-var links = document.getElementById("links").value;
-function submit(){
-    console.log(links);
+function submitBtn() {
+    var linksEntered = document.getElementById('links').value;
+    //to keep the values in the textbox after the button is pressed
+    document.getElementById('links').value = linksEntered;
+
+
+    chrome.storage.sync.set({
+            links: linksEntered
+        },
+        function () {
+            var status = document.getElementById('status');
+            status.textContent = 'Links entered were: ' + linksEntered;
+        });
+
 }
+document.getElementById('submitBtn').addEventListener('click', submitBtn);
+
+
+
