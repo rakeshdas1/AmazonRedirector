199ed8db8b77e436561671d18151b2ce3005140b
Rakesh Das <rakeshdaskowshik@gmail.com>
2016-06-15 23:54:00 -0500

    Fixing...

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-chrome.webRequest.onBeforeRequest.addListener(function (details) {
-    return detectRedirect(details);
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+  return detectRedirect(details);
 }, {
-    urls: ["<all_urls>"]
+  urls: ["<all_urls>"]
 }, ["blocking"]);
 
 function detectRedirect(details) {
 
   var url = details.url;
-  var http = "http://";
-  var https = "https://";
+  var http = "http:%2F%2F";
+  var https = "https:%2F%2F";
   var amazonurl = "www.amazon.com";
+  var smileamazonurl = "smile.amazon.com";
 
-    if (url.match(http + amazonurl) != null || url.match(amazonurl) || url.match(https + amazonurl) ) {
-            return {
-                redirectUrl: 'https://smile.amazon.com/?_encoding=UTF8&camp=1789&creative=390957&linkCode=ur2&tag=linustechtips-20'
-            };
-        };
+  console.log("URL = " + url);
+  if (url.match(http + amazonurl) || url.match(amazonurl) || url.match(https +
+      amazonurl)) {
+    return {
+      redirectUrl: 'https://amazon.com/?_encoding=UTF8&camp=1789&creative=390957&linkCode=ur2&tag=linustechtips-20'
+    };
+    console.log("Redirected to Amazon");
+
+  } else if (url.match(smileamazonurl)) {
+
+    return {
+      redirectUrl: 'https://smile.amazon.com/?_encoding=UTF8&camp=1789&creative=390957&linkCode=ur2&tag=linustechtips-20'
+    };
+    console.log("Redirected to Amazon Smile");
+
+  };
+
+
+
 }
